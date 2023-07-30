dd902e69c86fa1e31361b29c5e726216bd5b5b2f
Aaron Beckley <45105699+ambeckley@users.noreply.github.com>
2023-07-30 19:16:07 -0400

    Add files via upload

---
+++ FlashCardApp/FlashCardApp/FlashCardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardAppApp.swift
+//  FlashCardApp
+//
+//  Created by Aaron Beckley on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
